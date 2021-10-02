1979fe635ba711cfa3268a47df13613f42ed1b42
phuccuhp211 <phuccuhp211@gmail.com>
2021-10-02 20:05:35 +0700

    update

--- jquery/wa-v2.js
+++ jquery/wa-v2.js
@@ -2,14 +2,33 @@ $(function() {
     var width_window = $(window).width();                        
     console.log(width_window);
 
+/*    var h2p = $('.2-pic').height();
+    $('.l-pic').height(h2p);
+    var h2p_v2 = $('.2-pic-v2').height();
+    $('.l-pic-v2').height(h2p_v2);
+    var h2p_v3 = $('.2-pic-v3').height();
+    $('.l-pic-v3').height(h2p_v3);*/
+
+    var gtg = ($('.main-s-pic').height()*2)+30;
+    $('.box-pic').height(gtg)
+    var gtg2 = ($('.main-s-pic-v2').height()*2)+30;
+    $('.box-pic-v2').height(gtg2)
+    var gtg3 = ($('.main-s-pic-v3').height()*2)+15;
+    $('.box-pic-v3').height(gtg3)
+    
+    var hvd = $('.main-s-pic').height();
+    $('.s-vid').height(hvd);
+    var hvd2 = $('.main-s-pic-v2').height();
+    $('.s-vid-v2').height(hvd2);
+    var hvd3 = $('.main-s-pic-v3').height();
+    $('.s-vid-v3').height(hvd3);
+
     $(window).on('load', function() {
         $('.fx-1').addClass('fx-on');
         $('.fx-2').addClass('fx-on');
         $('.fx-3').addClass('fx-on');
         $('.fx-4').addClass('fx-on');
-    });
 
-    $(window).on('load', function() {
         if (width_window <= 767) {
             $('.fx-1').addClass('fx-on');
             $('.fx-2').addClass('fx-on');
@@ -68,22 +87,4 @@ $(function() {
             }
         }
     })
-
-    var h2p = $('.2-pic').height();
-    $('.l-pic').height(h2p);
-    var h2p_v2 = $('.2-pic-v2').height();
-    $('.l-pic-v2').height(h2p_v2);
-    var h2p_v3 = $('.2-pic-v3').height();
-    $('.l-pic-v3').height(h2p_v3);
-
-    var gtg = ($('.main-s-pic-v3').height()*2)+15;
-    $('.box-pic-v3').height(gtg)
-    
-
-    var hvd = $('.main-s-pic').height();
-    $('.s-vid').height(hvd);
-    var hvd2 = $('.main-s-pic-v2').height();
-    $('.s-vid-v2').height(hvd2);
-    var hvd3 = $('.main-s-pic-v3').height();
-    $('.s-vid-v3').height(hvd3);
 })
--- jquery/wa.js
+++ jquery/wa.js
@@ -2,6 +2,27 @@ $(function() {
     var width_window = $(window).width();                        
     console.log(width_window);
 
+/*    var h2p = $('.2-pic').height();
+    $('.l-pic').height(h2p);
+    var h2p_v2 = $('.2-pic-v2').height();
+    $('.l-pic-v2').height(h2p_v2);
+    var h2p_v3 = $('.2-pic-v3').height();
+    $('.l-pic-v3').height(h2p_v3);*/
+
+    var gtg = ($('.main-s-pic').height()*2)+30;
+    $('.box-pic').height(gtg)
+    var gtg2 = ($('.main-s-pic-v2').height()*2)+30;
+    $('.box-pic-v2').height(gtg2)
+    var gtg3 = ($('.main-s-pic-v3').height()*2)+15;
+    $('.box-pic-v3').height(gtg3)
+    
+    var hvd = $('.main-s-pic').height();
+    $('.s-vid').height(hvd);
+    var hvd2 = $('.main-s-pic-v2').height();
+    $('.s-vid-v2').height(hvd2);
+    var hvd3 = $('.main-s-pic-v3').height();
+    $('.s-vid-v3').height(hvd3);
+
     $(window).on('load', function() {
         $('.fx-1').addClass('fx-on');
         $('.fx-2').addClass('fx-on');
@@ -9,9 +30,7 @@ $(function() {
         $('.fx-4').addClass('fx-on');
         $('.fx-5').addClass('fx-on');
         $('.fx-6').addClass('fx-on');
-    });
 
-    /*$(window).on('load', function() {
         if (width_window <= 767) {
             $('.fx-1').addClass('fx-on');
             $('.fx-2').addClass('fx-on');
@@ -26,7 +45,7 @@ $(function() {
             $('.fx-11').addClass('fx-on');
             $('.fx-12').addClass('fx-on');
         }
-    });*/
+    });
     
     $(window).scroll(function(event){
         vtht = $('html, body').scrollTop();
@@ -81,23 +100,4 @@ $(function() {
             }
         }
     })
-
-    var h2p = $('.2-pic').height();
-    $('.l-pic').height(h2p);
-    var h2p_v2 = $('.2-pic-v2').height();
-    $('.l-pic-v2').height(h2p_v2);
-    var h2p_v3 = $('.2-pic-v3').height();
-    $('.l-pic-v3').height(h2p_v3);
-
-    var gtg = ($('.main-s-pic-v3').height()*2)+15;
-    $('.box-pic-v3').height(gtg)
-
-
-    var hvd = $('.main-s-pic').height();
-    $('.s-vid').height(hvd);
-    var hvd2 = $('.main-s-pic-v2').height();
-    $('.s-vid-v2').height(hvd2);
-    var hvd3 = $('.main-s-pic-v3').height();
-    $('.s-vid-v3').height(hvd3);
-        
 })
