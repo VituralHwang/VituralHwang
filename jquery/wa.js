a25c5cda5a95723df23ff233e7f8438919f55763
phuccuhp211 <phuccuhp211@gmail.com>
2021-10-02 22:33:25 +0700

    update

--- jquery/wa.js
+++ jquery/wa.js
@@ -9,13 +9,12 @@ $(function() {
         $('.box-pic-v2').height(gtg2)
         var gtg3 = ($('.main-s-pic-v3').height()*2)+15;
         $('.box-pic-v3').height(gtg3)
-
-        $('.fx-1').addClass('fx-on');
-        $('.fx-2').addClass('fx-on');
-        $('.fx-3').addClass('fx-on');
-        $('.fx-4').addClass('fx-on');
-        $('.fx-5').addClass('fx-on');
-        $('.fx-6').addClass('fx-on');
+        var hvd = $('.main-s-pic').height();
+        $('.s-vid').height(hvd);
+        var hvd2 = $('.main-s-pic-v2').height();
+        $('.s-vid-v2').height(hvd2);
+        var hvd3 = $('.main-s-pic-v3').height();
+        $('.s-vid-v3').height(hvd3);
 
         if (width_window <= 767) {
             $('.fx-1').addClass('fx-on');
@@ -33,26 +32,12 @@ $(function() {
         }
     });
 
-/*    var h2p = $('.2-pic').height();
-    $('.l-pic').height(h2p);
-    var h2p_v2 = $('.2-pic-v2').height();
-    $('.l-pic-v2').height(h2p_v2);
-    var h2p_v3 = $('.2-pic-v3').height();
-    $('.l-pic-v3').height(h2p_v3);*/
-
-    var gtg = ($('.main-s-pic').height()*2)+30;
-    $('.box-pic').height(gtg)
-    var gtg2 = ($('.main-s-pic-v2').height()*2)+30;
-    $('.box-pic-v2').height(gtg2)
-    var gtg3 = ($('.main-s-pic-v3').height()*2)+15;
-    $('.box-pic-v3').height(gtg3)
-    
-    var hvd = $('.main-s-pic').height();
-    $('.s-vid').height(hvd);
-    var hvd2 = $('.main-s-pic-v2').height();
-    $('.s-vid-v2').height(hvd2);
-    var hvd3 = $('.main-s-pic-v3').height();
-    $('.s-vid-v3').height(hvd3);
+    $('.fx-1').addClass('fx-on');
+    $('.fx-2').addClass('fx-on');
+    $('.fx-3').addClass('fx-on');
+    $('.fx-4').addClass('fx-on');
+    $('.fx-5').addClass('fx-on');
+    $('.fx-6').addClass('fx-on');
     
     $(window).scroll(function(event){
         vtht = $('html, body').scrollTop();
