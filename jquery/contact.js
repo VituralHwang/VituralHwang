d11a2ce1ca0c06c20186b300b3a55b42df9a9383
phuccuhp211 <phuccuhp211@gmail.com>
2021-10-03 11:19:29 +0700

    update

--- jquery/contact.js
+++ jquery/contact.js
@@ -1,8 +1,8 @@
 function cpt(id) {
-		var cp = document.createRange();
-		cp.selectNode(document.getElementById(id));
-		window.getSelection().removeAllRanges();
-		window.getSelection().addRange(cp);
-		document.execCommand('copy');
-		window.getSelection().removeAllRanges();
-	}
+	var cp = document.createRange();
+	cp.selectNode(document.getElementById(id));
+	window.getSelection().removeAllRanges();
+	window.getSelection().addRange(cp);
+	document.execCommand('copy');
+	window.getSelection().removeAllRanges();
+}
